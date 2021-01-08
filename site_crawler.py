4de58027cc855074fa27605a4ad88fa58800e2b2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-08 18:57:08 +0200

    refactor: Improve main logic and split into main and process_sitemap functions

--- site_crawler.py
+++ site_crawler.py
@@ -7,41 +7,66 @@ from usp.tree import sitemap_tree_for_homepage as site_map_tree
 from etc import config
 from lib import analysis_report, configure_browser, logging, process_arguments, process_url
 
-args = process_arguments()
-browser = configure_browser()
 
-exclude_urls = config.exclude_paths
+def process_sitemap(browser, site_url, max_urls, show_list, show_report, exclude_urls):
+    results = []
 
-processed_urls = []
-data = []
-idx = 0
+    counter = 0
+    processed_urls = []
 
-try:
-    if args.url:
-        data.append(process_url(browser, args.url))
-    else:
-        target_url = args.siteurl if args.siteurl else config.target_url
-        site_map = site_map_tree(target_url)
-        for page in site_map.all_pages():
-            if args.max > 0:
-                idx = idx + 1
-                if idx > args.max:
-                    break
+    target_url = site_url if site_url else config.target_url
+    site_map = site_map_tree(target_url)
 
-            if page.url not in processed_urls and page.url not in exclude_urls:
-                processed_urls.append(page.url)
+    for page in site_map.all_pages():
+        if max_urls:
+            counter += 1
+            if counter > max_urls:
+                break
 
-                if args.list:
-                    print(page.url)
+        if page.url in processed_urls or page.url in exclude_urls:
+            continue
 
-                else:
-                    data.append(process_url(browser, page.url))
+        processed_urls.append(page.url)
 
-    if args.report:
-        analysis_report(data)
+        if show_list:
+            print(page.url)
+        if show_report:
+            result = process_url(browser, page.url)
+            results.append(result)
 
-except Exception as e:
-    logging.error('Exception: Error loading: %s', e)
+    return results
 
-finally:
-    browser.quit()
+
+def main(args):
+    """
+    Command-line entrypoint to process a URL or sitemap and show a report if needed.
+    """
+    browser = configure_browser()
+
+    try:
+        if args.url:
+            result = process_url(browser, args.url)
+            results = [result]
+        else:
+            results = process_sitemap(
+                browser,
+                site_url=args.siteurl,
+                max_urls=args.max,
+                show_list=args.list,
+                show_report=args.report,
+                exclude_urls=config.exclude_paths,
+            )
+    except Exception as e:
+        logging.error('Exception: Error loading: %s', e)
+    finally:
+        browser.quit()
+
+    if args.report or args.url:
+        if results:
+            analysis_report(results)
+        else:
+            logging.warn('No processed URLs to report on')
+
+
+if __name__ == '__main__':
+    main(process_arguments())
